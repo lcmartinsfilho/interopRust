9b6f020587de83fa0e2ac99044f90c66408e680f
Luiz Carlos Martins Filho <lcmf@pm.me>
2022-10-30 17:31:32 -0300

    feat - testing Boa. First test.

--- boa_js_rust/src/main.rs
+++ boa_js_rust/src/main.rs
@@ -1,3 +1,19 @@
+use boa_engine::Context;
+
 fn main() {
-    println!("Hello, world!");
+    let js_code = "console.log('Hello World from a JS code string!')";
+
+    // Instantiate the execution context
+    let mut context = Context::default();
+
+    // Parse the source code
+    match context.eval(js_code) {
+        Ok(res) => {
+            println!("{}", res.to_string(&mut context).unwrap());
+        }
+        Err(e) => {
+            // Pretty print the error
+            eprintln!("Uncaught {}", e.display());
+        }
+    };
 }
